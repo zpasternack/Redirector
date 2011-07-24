0b243bcef4aaecdf5ddf39398b2af8b61808252a
Zacharias Pasternack <zacharias.pasternack@gmail.com>
2011-07-23 21:38:47 -0700

    Initial Commit

---
+++ Redirector/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Redirector
+//
+//  Created by Bradley Lindauer on 7/23/11.
+//  Copyright 2011 WildPackets, Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
